01a181ad08bd4d5895f90d4208cae31ba3c7f099
MyNNeiei <kullananzaza@gmail.com>
2024-10-02 04:30:40 +0700

    nncommit : login finish not Authen

--- bell_Space/belle_space/forms.py
+++ bell_Space/belle_space/forms.py
@@ -1,11 +1,12 @@
 from datetime import date,datetime
 from django import forms
-from django.forms import ModelForm, SplitDateTimeField
+from django.forms import EmailInput, ModelForm, PasswordInput, Select, SplitDateTimeField
 from django.forms.widgets import Textarea, TextInput, DateInput
 from django.core.exceptions import ValidationError
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
 
+        
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
@@ -16,6 +17,13 @@ class UserRegisterForm(UserCreationForm):
             "first_name",
             "last_name",
         ]
+        widgets = {
+            "username" : TextInput(attrs={'class': 'border border-gray-300 rounded px-3 py-2 w-full', 'placeholder': 'Username'}),
+            "password" : PasswordInput(attrs={'class': 'border border-gray-300 rounded px-3 py-2 w-full', 'placeholder': 'Password'}),
+            "email" : EmailInput(attrs={'class': 'border border-gray-300 rounded px-3 py-2 w-full', 'placeholder': 'Username'}),
+            "first_name" : TextInput(attrs={'class': 'border border-gray-300 rounded px-3 py-2 w-full', 'placeholder': 'First_name'}),
+            "last_name" : TextInput(attrs={'class': 'border border-gray-300 rounded px-3 py-2 w-full', 'placeholder': 'Last_name'}),
+        }
 class UserdetailRegisterForm(forms.ModelForm):
     class Meta:
         model = UsersDetail
@@ -23,13 +31,17 @@ class UserdetailRegisterForm(forms.ModelForm):
             'gender',
             'phone_number',
             'birth_date',
-            'image_profile'
         ]
+        widgets = {
+            "birth_date" : DateInput(attrs={"type": "date", "class" : "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"}),
+            "gender" : Select(attrs={'class': 'border border-gray-300 rounded px-3 py-2 w-full'}),
+            "phone_number" : TextInput(attrs={'class': 'border border-gray-300 rounded px-3 py-2 w-full', 'placeholder': 'กรอกเบอร์โทรศัพท์'}),
+            }
     def clean_birthdate(self):
         cleaned_data = super().clean()
         birth_date = cleaned_data.get("birth_date")
         birth_now = datetime.now().date()
-        if birth_date > birth_now:
+        if birth_date > birth_now and birth_date == birth_now:
             raise ValidationError(
                     "birth_date ว่าจะต้องไม่เป็นวันในอนาคต"
             )
