799e8a2917cef711966b5120668a0f72f6d7069f
bamibibi <Natthacha9876@gmail.com>
2024-10-09 23:54:43 +0700

    fix form & mutiple choices not yet

--- bell_Space/belle_space/urls.py
+++ bell_Space/belle_space/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     path("login/", views.LoginFormView.as_view(), name="login_form"),
     path("logout/", views.Logout.as_view(), name="logout"),
     path("appointment/", views.AppointmentView.as_view(), name="appointment"),
-    
     path("appointment/<int:detail>", views.AppointmentDetailView.as_view(), name="appointment_detail"),
     path("appointment/<int:id>/delete/", views.AppointmentView.as_view(), name="appointment_delete"),
-    path("appointment/<int:appointment_id>/<int:staff_id>/add/", views.AppointmentDetailView.as_view(), name="appointment_detail_add"),
-    path("appointment/appointment_form/", views.AppointmentFormView.as_view(), name="appointment_form"),
+    path("appointment/<int:appointment_id>/<int:staff_id>/add/", views.AppointmentDetailView.as_view(), name="appointment_detail_add"), 
+    path('appointment/appointment/', views.AppointmentFormView.as_view(), name='appointment_form'),
+    path('appointment/appointment/load_services/', views.load_services, name='load_services'),  # Load services with the new function
+
+
 ]
+
