d198fa5f2638bcd5e304762d89224417b3563aef
MyNNeiei <kullananzaza@gmail.com>
2024-10-10 23:20:31 +0700

    nncommittoswitchmain

--- bell_Space/belle_space/urls.py
+++ bell_Space/belle_space/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
+
 urlpatterns = [
     # ex: /polls/
     path("", views.IndexView.as_view(), name="home"),
